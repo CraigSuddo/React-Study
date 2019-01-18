59526e0e9993cbe7c868af46bcef0e0c448427e2
craigsuddo <craigsuddo@gmail.com>
2019-01-18 19:17:35 +0000

    adding components

---
+++ assignment-1/src/UserInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserInput extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <input type="text" id="txt"></input>
+        );
+    }
+}
+
+export default UserInput;
---
+++ assignment-1/src/UserOutput.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class UserOutput extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <p>My name is NAME</p>
+                <p>I like.... </p>
+            </div>
+        );
+    }
+}
+
+export default UserOutput;
